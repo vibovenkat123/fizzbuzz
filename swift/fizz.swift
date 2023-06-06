94d6d7c70d8ed41663d1462f2d188a6c0fbf40cc
Vaibhav Venkat <vibovenkat@gmail.com>
2023-06-06 09:57:30 -0500

    feat: swift

---
+++ swift/fizz.swift
@@ -0,0 +1,13 @@
+
+for i in 1...100 {
+    switch (i % 3 == 0, i % 5 == 0) {
+    case (true, false):
+        print("fizz")
+    case (false, true):
+        print("buzz")
+    case (true, true):
+        print("fizzbuzz")
+    case (false, false):
+        print(i)
+    }
+}
